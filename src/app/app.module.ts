81d3d12dfad501cf0b08a7aa34cd27b1a2e827ab
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-06 10:39:10 -0300

    [add]: shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, MdbCheckboxModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MdbCheckboxModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
