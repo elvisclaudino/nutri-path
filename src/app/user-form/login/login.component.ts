2ad313e656b278fb76fbbf31cae511bb0849ff1d
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-07 16:07:02 -0300

    [add]: login completed

--- src/app/user-form/login/login.component.ts
+++ src/app/user-form/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
+import Swal from 'sweetalert2';
+
 import { FormBaseComponent } from 'src/app/shared/components/form-base/form-base.component';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +12,10 @@ import { FormBaseComponent } from 'src/app/shared/components/form-base/form-base
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent extends FormBaseComponent {
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService
+  ) {
     super();
   }
 
@@ -20,5 +26,33 @@ export class LoginComponent extends FormBaseComponent {
     });
   }
 
-  submit() {}
+  submit() {
+    this.loginService
+      .userLogin(this.form.value.email, this.form.value.password)
+      .subscribe(
+        (user) => {
+          if (user) {
+            Swal.fire(
+              'Login realizado!',
+              `Olá ${user.name} ${user.lastName}`,
+              'success'
+            );
+          } else {
+            Swal.fire(
+              'Erro ao fazer login!',
+              `Email ou senha incorretos! Tente novamente.`,
+              'error'
+            );
+          }
+        },
+        (error) => {
+          console.log('Erro ao fazer login:', error);
+          Swal.fire(
+            'Erro ao fazer login!',
+            `Tente novamente mais tarde.`,
+            'error'
+          );
+        }
+      );
+  }
 }
