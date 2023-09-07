10a2c9b52277e3815d22634226441315e7a973f7
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-07 03:06:28 -0300

    [add] login style

--- src/app/user-form/login/login.component.ts
+++ src/app/user-form/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { FormBaseComponent } from 'src/app/shared/components/form-base/form-base.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent extends FormBaseComponent {
+  constructor(private formBuilder: FormBuilder) {
+    super();
+  }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  submit() {}
 }
