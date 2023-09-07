7992f6a4c918348483df20bd5da6e2e922ab0078
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-07 16:48:12 -0300

    [add]: logout confirm

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AuthService } from './../../../user-form/services/auth.service';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
+  isLoggedIn = false;
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService
+      .isLoggedIn$()
+      .subscribe((isLoggedIn) => (this.isLoggedIn = isLoggedIn));
+  }
 
   shouldShowLoginButton(path: string): boolean {
     const currentRoute = this.router.url;
 
     return currentRoute !== path;
+  }
+
+  logout() {
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Deseja mesmo sair?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Você saiu!', 'Voltando a tela de login.', 'success');
+        this.authService.logout();
+        this.router.navigate(['/home/login']);
+      }
+    });
   }
 }
