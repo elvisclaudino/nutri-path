708432003d06b81caef0fe0ca94be637d5950b6f
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-09-08 01:15:11 -0300

    [update]: fix filter by id

--- src/app/dashboard/services/diet.service.ts
+++ src/app/dashboard/services/diet.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 
+import { AuthService } from 'src/app/shared/services/auth.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class DietService {
   private apiUrl = 'http://localhost:3000/dieta';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   addFoodToDiet(food: any) {
     return this.http.post(this.apiUrl, food);
@@ -17,9 +19,11 @@ export class DietService {
   getDietSortedByTime(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl).pipe(
       map((diet) =>
-        diet.sort((a, b) => {
-          return a.time.localeCompare(b.time);
-        })
+        diet
+          .filter((food) => food.userId === this.authService.getUserId())
+          .sort((a, b) => {
+            return a.time.localeCompare(b.time);
+          })
       )
     );
   }
@@ -28,7 +32,10 @@ export class DietService {
     return this.http.get<any[]>(this.apiUrl).pipe(
       map((diet) =>
         diet.filter((food) => {
-          return food.category === categoria;
+          return (
+            food.category === categoria &&
+            food.userId === this.authService.getUserId()
+          );
         })
       )
     );
